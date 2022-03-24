e2c1d39fca0ac0eb319a5486210291b53dc62e78
DevMohamedElshazly <moshazly416@gmail.com>
2022-03-24 02:33:40 +0200

    add-home

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+/*=============== SHOW MENU ===============*/
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+/*=============== SHOW CART ===============*/
+
+/*===== CART SHOW =====*/
+/* Validate if constant exists */
+
+/*===== CART HIDDEN =====*/
+/* Validate if constant exists */
+
+/*=============== SHOW LOGIN ===============*/
+
+/*===== LOGIN SHOW =====*/
+/* Validate if constant exists */
+
+/*===== LOGIN HIDDEN =====*/
+/* Validate if constant exists */
+
+/*=============== HOME SWIPER ===============*/
+var homeSwiper = new Swiper(".home-swiper", {
+  spaceBetween: 30,
+  loop: "true",
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+/*=============== NEW SWIPER ===============*/
+
+/*=============== SHOW SCROLL UP ===============*/
+
+/*=============== LIGHT BOX ===============*/
+
+/*=============== QUESTIONS ACCORDION ===============*/
+
+/*=============== STYLE SWITCHER ===============*/
